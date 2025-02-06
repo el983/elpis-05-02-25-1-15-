6c3475d1319775b4b5f98a89964a1917e24fe502
el983 <elpislearningconsultantsllp88@gmail.com>
2025-02-06 20:15:17 +0530

    Add files via upload

---
+++ src/DolphinSection.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./DolphinSection.css";
+
+const DolphinSection = () => {
+  return (
+    <section className="dolphin-section">
+      <div className="dolphin-gif-container">
+        <div className="dolphin-text-overlay">
+          <p className="dolphin-overlay-heading">Core Values:</p>
+          <ul className="dolphin-overlay-list">
+            <li>Excellence</li>
+            <li>Trust</li>
+            <li>Collaboration</li>
+            <li>Integrity</li>
+            <li>Leadership</li>
+            <li>Accountability</li>
+            <li>Passion</li>
+          </ul>
+        </div>
+        <img 
+          src="/dolphin.gif" 
+          alt="Dolphin swimming" 
+          className="dolphin-gif"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default DolphinSection;
