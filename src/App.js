d4a6b50771502f133907116863eb59f4e40eafc4
el983 <elpislearningconsultantsllp88@gmail.com>
2025-02-21 11:03:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -216,7 +216,7 @@ function App() {
       <div style={{height: '20vh'}}></div>
       <div class="video-card">
       <img src="https://img.youtube.com/vi/2VY-GpXaAlQ/maxresdefault.jpg" alt="Video Thumbnail" class="thumbnail" />
-      <iframe src="https://www.youtube.com/watch?v=gxGbsXhhZeI" frameborder="0" allow="autoplay; encrypted-media" class="video"></iframe>
+      <iframe src="https://youtu.be/gxGbsXhhZeI?si=H6GzZgY19Lm_u4rV" frameborder="0" allow="autoplay; encrypted-media" class="video"></iframe>
       </div>
 
       {/* Who we are and Vision  */}
